9528f6673e07f404b3fcbf13f1ba8c7d1e4ee7ec
Eleandro Duzentos <eleandro@inbox.ru>
2020-12-10 04:56:20 +0100

    Added Searcher

---
+++ lib/src/widgets/searcher.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Searcher extends StatelessWidget {
+  final Function(Offset offset) onSearch;
+
+  const Searcher({
+    Key key,
+    this.onSearch,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(.1),
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: Colors.black.withOpacity(.2),
+            width: 2,
+          ),
+        ),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
